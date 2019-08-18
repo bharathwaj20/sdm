cbc305ff5f1342c4885064f4291ceba6b7b2e960
Bharathwaj <bharathwaj20@gmail.com>
2019-08-18 19:45:32 +1200

    mocha

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
 var calculator = require('../index');
-describe('calculator', function() {
-    describe('add function', function() {
-        it('adds numbers', function () {
+describe('MERN APPLICATION', function() {
+    describe('UNIT TEST', function() {
+        it('Success', function () {
             var result = calculator.add(1, 1);
             assert.equal(result, 2);
         });
